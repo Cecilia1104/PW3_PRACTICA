97dadd12e1bfae9022a2aad283246ac71cd3fbf4
Cecilia1104 <ce.gisele84@gmail.com>
2013-08-30 16:27:29 -0300

    Agregue un switch para elegir decimal o binario

--- 28d08/WebApplication2/calculadora.ascx.cs
+++ 28d08/WebApplication2/calculadora.ascx.cs
@@ -27,7 +27,16 @@ namespace WebApplication2
         y si es binario a binario*/
         protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
         {
-          //lblresultado.Text = Convert.ToString(resultado, 2); //para binario
+            switch (DropDownList1)
+            {
+                case "decimal":
+                    lblresultado.Text = Convert.ToString(resultado);
+                    break;
+                case "binario":
+                      lblresultado.Text = Convert.ToString(resultado, 2); 
+                    break;
+            }
+            
 
         
         }
