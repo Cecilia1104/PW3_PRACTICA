78592a2dbde5174d90463db27d48e200283b5a88
Cecilia1104 <Cecilia04@Cecilia.com>
2013-09-29 20:22:38 -0300

    Ejercicio modificado

---
+++ 27d08/27d08/Class3.cs
@@ -0,0 +1,52 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+
+//namespace _27d08
+//{
+//    class Class3  //conversiones de tipo de datos: ir comentando por partes para probarlo
+//    {
+
+
+//        static void Main(string[] args)
+//        {
+
+
+//            string palabra = "hola";
+//            Convertir(palabra);
+//            ConvertirAEntero(palabra);
+//            ConvertirTres(palabra);
+
+
+//        }
+//        //Convertir con Parse
+//        static void Convertir(string palabra)
+//        {
+//            int valor = int.Parse(palabra);
+//            Console.Write("Conversion");
+//            Console.WriteLine(valor);
+//            Console.Read();
+
+//        }
+//        //Convertir version basica -  
+//        static void ConvertirAEntero(string palabra)
+//        {
+//            int devuelve = System.Convert.ToInt32(palabra);
+//            Console.WriteLine(devuelve);
+//            Console.Read();
+//        }
+//        //Convertir con TryParse
+//        static void ConvertirTres(string palabra)
+//        {
+//            int final;
+//            bool ret = int.TryParse(palabra, out final);
+//            Console.WriteLine(ret);
+//            Console.Read();
+
+
+//        }
+//    }
+//}
+          
+
---
+++ 27d08/27d08/Class4.cs
@@ -0,0 +1,21 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+
+//namespace _27d08
+//{
+//    class Class4   //Uso del Foreach
+//    {
+//        static void Main(string[] args)
+//        {
+//            char[,] letras ={
+//                                  {'a','b','c'},{'r','t','q'}
+//                              };
+//            foreach (char elementos in letras)
+//                Console.WriteLine(elementos);
+
+//            Console.Read();
+//        }
+//    }
+//}
